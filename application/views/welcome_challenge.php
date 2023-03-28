e48447b4682e96cda3153ee6eeaa0a2e27ce4781
wtfranco22 <francofranco_96@hotmail.com>
2023-03-27 23:21:02 -0300

    agregando un mensaje de bienvenida

--- application/views/welcome_challenge.php
+++ application/views/welcome_challenge.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to challenge</title>
 
 	<style type="text/css">
 
@@ -68,18 +68,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Bienvenido al desafio!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>La pagina solo mantendra el cartel de bienvenida para los usuarios.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>Datos random: al tener instalado docker y creado el archivo docker-compose.yml</p>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+		<p>Ejecute el siguiente comando y pude lograr levantar el proyecto sin necesidad de tener nuevas herramientas instaladas</p>
+		<code>curl -LO https://raw.githubusercontent.com/bitnami/containers/main/bitnami/codeigniter/docker-compose.yml</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Ya que estoy explorando aún lo que es docker y docker compose, sigo utilizando <a href="https://hub.docker.com/">Docker hub</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
